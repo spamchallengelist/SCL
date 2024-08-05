e387560a656b345c610affcc8312449e8cddd2cd
spamchallengelist <courtappendix@gmail.com>
2024-08-05 01:20:35 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -14,7 +14,7 @@ export function score(rank, percent, minPercent) {
     if (rank > 100) {
         return 0;
     }
-    if (rank > 74 && percent < 100) {
+    if (rank > 75 && percent < 100) {
         return 0;
     }
 
