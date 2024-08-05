c99757180dd828a925c90e68bf125418444a14b9
spamchallengelist <courtappendix@gmail.com>
2024-08-05 00:57:08 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-37.9975*Math.pow(rank-1, 0.4) + 250) *
+    let score = (-39.5*Math.pow(rank-1, 0.4) + 250) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
