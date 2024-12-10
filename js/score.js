be1b6cd5fdf93c80f34925dfd70ad7a822fe7c52
Grilled <73136925+GrilledTG@users.noreply.github.com>
2024-12-10 16:15:19 -0600

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,10 +11,10 @@ const scale = 2;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 75) {
+    if (rank > 100) {
         return 0;
     }
-    if (rank > 75 && percent < 100) {
+    if (rank > 100 && percent < 100) {
         return 0;
     }
 
