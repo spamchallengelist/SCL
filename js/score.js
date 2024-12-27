a6cb55478587e5b8c7e801c2eb71e383350b0987
Grilled <73136925+GrilledTG@users.noreply.github.com>
2024-12-27 13:34:01 -0600

    Update score.js

--- js/score.js
+++ js/score.js
@@ -14,7 +14,6 @@ export function score(rank, percent, minPercent) {
     if (rank > 100) {
         return 0;
     }
-
     if (rank > 100 && percent < 100) {
         return 0;
     }
@@ -25,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-41.5*Math.pow(rank-1, 0.4) + 300) *
+    let score = (-41.5*Math.pow(rank-1, 0.4) + 275) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
