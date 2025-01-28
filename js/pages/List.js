4a19410eac2ac0c88f573332fd789f9cea6315ad
Grilled <73136925+GrilledTG@users.noreply.github.com>
2025-01-27 21:23:16 -0600

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -124,6 +124,9 @@ export default {
                     <p>
                          It may say Spam "Challenge" List, however there is not really a time limit.
                     </p>
+                    <p>
+                    All hardware is able to be used, though it is level specific. For example, you can use logitech g512 to beat pepino. Though for a level like Why The Spam, you have to use a near capless device (15ms debounce or less). A list of hardware and its appropriate debounce time will most likely develop over time. Levels beaten with capped devices will be placed according to how hard it was on the capped device. If a level is beaten with a device with a lower debounce delay, the level will be verified with that device as long as the difference is greater than a 5ms debounce delay. This also means the device with a higher debounce delay will not be allowed in completions anymore. An example would be beating garbanzo on k55 then later it gets beaten on sayo device, garbanzo will be ranked according to how hard it is on sayo device and will no longer be allowed to be beaten on k55.
+                    </p>
                 </div>
             </div>
         </main>
